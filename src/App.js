a6721fabede6298718d87a65e84d2ff4c6a984c2
ElgissioFranito <mistera.inwy@gmail.com>
2024-12-10 12:29:27 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logoReact from './logo.svg';
 import logoAngular from './angular.svg'
 import LogoVue from './link-icon.png';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const frameworkz = {
-  react: {
+const frameworkz = [
+  {
     name: "React",
     version: "18.2.0",
     description: "A JavaScript library for building user interfaces",
@@ -12,7 +12,7 @@ const frameworkz = {
     link: "https://reactjs.org",
     logo: logoReact
   },
-  angular: {
+  {
     name: "Angular",
     version: "15.2.0",
     description: "A framework for building single-page applications",
@@ -20,7 +20,7 @@ const frameworkz = {
     link: "https://angular.io",
     logo: logoAngular
   },
-  vue: {
+  {
     name: "Vue",
     version: "3.2.45",
     description: "A progressive JavaScript framework for building user interfaces",
@@ -28,7 +28,7 @@ const frameworkz = {
     link: "https://vuejs.org",
     logo: LogoVue
   }
-};
+];
 
 
 const Myname = (props) => {
@@ -36,13 +36,13 @@ const Myname = (props) => {
 }
 
 
-const Framework = (props) => {
+const Framework = ({ name, description, author, link }) => {
   return (
-    <div className='flex flex-col gap-2 hover:bg-slate-100 cursor-pointer w-96 px-5'>
-      <div className='text-2xl font-bold text-red-800'> {props.framework.name} </div>
-      <div className='text-sm'> Description : {props.framework.description} </div>
-      <div className='text-sm'> Author : {props.framework.author} </div>
-      <a href={props.framework.link}> Link :  {props.framework.link} </a>
+    <div className='flex flex-col gap-2 hover:bg-slate-100 cursor-pointer w-96 px-5 py-3 rounded-lg'>
+      <div className='text-2xl font-bold text-red-800'> {name} </div>
+      <div className='text-sm'> Description : {description} </div>
+      <div className='text-sm'> Author : {author} </div>
+      <a href={link}> Link :  {link} </a>
     </div>
   )
 }
@@ -55,14 +55,31 @@ const Clock = (props) => {
 }
 
 
+
+
 const App = () => {
 
-  const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(new Date().toLocaleTimeString());   // signal
+  const [isVisible, setVisible] = useState(true);   // signal
 
-  const logoHeader = frameworkz.react.logo;
+  const logoHeader = frameworkz[0].logo;
 
-  const onChangeLogo = () => {
-    console.log("haha");
+  useEffect(() => {                       
+    // OnInit
+    const interval = setInterval(() => {
+      setDate(new Date().toLocaleTimeString());
+    }, 1000);
+    
+    // OnDestroy --- return a function
+    return () => clearInterval(interval);
+  }, []);                                         // empty dependencies for the mount
+
+  const onChangeLogo = (value) => {
+    console.log(value);
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!isVisible);
   }
 
   return (
@@ -72,13 +89,20 @@ const App = () => {
         <img src={logoHeader} className="App-logo" alt="logo" width="100px" />
 
         <Myname name="Elgissio Franito" />
+ 
+        {/* voir uniquement l'heure si c'est visible=true */}
+        {isVisible && <Clock newDate={date} /> }
+        {isVisible && <button onClick={() => setDate(new Date().toLocaleTimeString())} className='border bg-green-400 px-5 py-2 rounded-md w-52'> Update Time </button> }
 
-        <Clock newDate={date} />
-        <button onClick={() => setDate(new Date())} className='border bg-green-400 px-5 py-2 rounded-md w-52'> update Date </button>
+        {/* ternaire */}
+        <button onClick={toggleVisibility} className='border bg-green-400 px-5 py-2 rounded-md w-52'> {isVisible ? "Hide Time" : "Show Time"} </button>
 
-        <Framework framework={frameworkz.react} onClick={onChangeLogo} /> <br />
-        <Framework framework={frameworkz.angular} onClick={onChangeLogo} /> <br />
-        <Framework framework={frameworkz.vue} onClick={onChangeLogo} /> <br />
+        {
+          frameworkz.map((value, index) => {
+            // return <Framework key={index} {...value} />        // à éviter
+            return <Framework key={value.link} {...value} onClick={ () => console.log(value.logo)} />
+          })
+        }
 
       </header>
     </div>
